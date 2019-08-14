62d1eecd9edfef1b35b792a5daebd9a2218dcac9
Pedro <pedroecomp@gmail.com>
2019-08-13 21:11:20 -0300

    Full backend

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,18 +1,13 @@
 const express = require('express');
+const DevController = require('./controllers/DevController');
+const LikeController = require('./controllers/LikeController');
+const DislikeController = require('./controllers/DislikeController');
 
 const routes =  express.Router();
+routes.get('/devs',DevController.index);
 
-routes.get('/', (req, res) => {
-    req.query.name;
-    return res.json({
-        message: `Ola ${req.query.name}`
-    });
-});
-
-routes.post('/devs',(req,res)=>{
-    console.log(req.body);
-    return res.json({ok:true});
-});
-
+routes.post('/devs',DevController.store);
+routes.post('/devs/:devId/likes',LikeController.store);
+routes.post('/devs/:devId/dislikes',DislikeController.store);
 
 module.exports= routes;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const routes = require('./routes');
 const mongoose = require('mongoose');
+const mongoose = require('cors');
+
 const server = express();
 mongoose.connect('mongodb+srv://pedroAdmin:macarao08@cluster0-o8exh.mongodb.net/test?retryWrites=true&w=majority', 
 {useNewUrlParser:true}
 );
 
+server.use(cores());
 server.use(express.json());
 server.use(routes);
+
 server.listen(3333);
